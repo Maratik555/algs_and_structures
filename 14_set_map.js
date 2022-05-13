c2db9248d4ad7ee2f6cd69e5cfc9934227ba5093
Маратик555 <amr404@bk.ru>
2022-05-13 17:19:37 +0300

    ok

---
+++ 14_set_map.js
@@ -0,0 +1,16 @@
+const map = new Map()
+const objKey = {id:5}
+map.set(objKey, "marat")
+
+console.log(map.get(objKey));
+
+const set = new Set()
+
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(4)
+set.add(3)
+console.log(set)
